ff56b10c48000fc87d51a8fc0fffa255f342d45b
MathiBou <mathilde.bourjot@gmail.com>
2016-10-30 18:43:59 +0100

    fenetre

--- fenetre.py
+++ fenetre.py
@@ -1,7 +1,7 @@
-from tkinter import *
-fenetre1 = TK() # instance de la classe TK
+from Tkinter import *
+fenetre1 = Tk() # instance de la classe TK
 texte1 = Label(fenetre1, text="Coucou !", fg='red')
-texte1.pack() # taille ajustée
-bouton1 = Button(fenetre1, text='Quitter', command = fenetre1.destroy) # pas appel à la méthode
+texte1.pack() # taille ajustee
+bouton1 = Button(fenetre1, text='Quitter', command = fenetre1.destroy)
 bouton1.pack()
-fenetre1.mainloop() # démarrage du réceptionnaire d'événements
+fenetre1.mainloop() # demarrage du receptionnaire d'evenements
